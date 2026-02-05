94d764606a8a8fcda9a0f53ebcda3d0dfc731289
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-05 20:20:05 +0600

    Add files via upload

---
+++ yes_no.py
@@ -0,0 +1,15 @@
+t  = int(input())
+
+for _ in range(t):
+    
+    a = input()
+    
+    a = a.upper()
+    
+    if a == "YES":
+        print("YES")
+        
+    else:
+        print("NO")
+        
+        
